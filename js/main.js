7bb19a70ca413278215c81d1f31364d11872fd87
vilivonka <gromootvod@lenta.ru>
2024-08-11 12:53:53 +0300

    all page

--- js/main.js
+++ js/main.js
@@ -36,7 +36,7 @@
 
 (() => {
 
-  const anchors = document.querySelectorAll('a[href*="#"]')
+  const anchors = document.querySelectorAll('a[href^="#"]')
 
   for (let anchor of anchors) {
     anchor.addEventListener('click', function (e) {
